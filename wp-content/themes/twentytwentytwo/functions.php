3c8a426d3bc157092176eb4a3abb074d35cfb8c1
Segundo Leon <segundoleon@msn.com>
2022-11-15 23:33:06 -0500

    options

--- wp-content/themes/twentytwentytwo/functions.php
+++ wp-content/themes/twentytwentytwo/functions.php
@@ -65,3 +65,8 @@ add_action( 'wp_enqueue_scripts', 'twentytwentytwo_styles' );
 
 // Add block patterns
 require get_template_directory() . '/inc/block-patterns.php';
+
+/**
+ * ACF Options.
+ */
+require 'inc/acf-options.php';
